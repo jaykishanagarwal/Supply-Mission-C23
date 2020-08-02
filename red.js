328c971a2f49585e8b69ad1532b30ebc30e8f081
jaykishanagarwal <68913797+jaykishanagarwal@users.noreply.github.com>
2020-08-02 19:43:20 +0530

    Add files via upload

---
+++ red.js
@@ -0,0 +1,19 @@
+class red {
+    constructor(x,y,width,height){
+        var option={
+            isStatic : true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+
+
+    }
+    display(){
+        var position=this.body.position;
+        rectMode(CENTER);
+        fill(255,0,0);
+        rect(position.x,position.y,this.width,this.height);
+    }
+};
